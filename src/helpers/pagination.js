5978b740a0ef74867a70d7a57e05f144d90d4f70
Dmytro Kitseliuk <dima_kitseliuk@ukr.net>
2026-03-26 16:45:53 +0200

    add user routes

---
+++ src/helpers/pagination.js
@@ -0,0 +1,17 @@
+// src/helpers/pagination.js
+
+const getPagination = (query, config) => {
+  const { defaultPage, defaultPerPage } = config;
+  const page = Number(query.page) || defaultPage;
+  const perPage = Number(query.perPage) || defaultPerPage;
+  const skip = (page - 1) * perPage;
+
+  return {
+    page,
+    perPage,
+    skip,
+    limit: perPage,
+  };
+};
+
+export { getPagination };
--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from 'express';
-import cors from "cors";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { errors } from 'celebrate';
-import { logger } from "./middleware/logger.js";
-import { notFoundHandler } from "./middleware/notFoundHandler.js";
-import { errorHandler } from "./middleware/errorHandler.js";
-import { getEnvVar } from "./helpers/getEnvVar.js";
-import { ENV_VARS } from "./constants/envVars.js";
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { getEnvVar } from './helpers/getEnvVar.js';
+import { ENV_VARS } from './constants/envVars.js';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import authRoutes from './routes/authRoutes.js';
 import feedbackRoutes from './routes/feedbackRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
@@ -31,6 +32,7 @@ app.get('/', (req, res) => {
 
 app.use(authRoutes);
 app.use(feedbackRoutes);
+app.use(userRoutes);
 
 //Middleware - 404 - Route not found
 app.use(notFoundHandler);
