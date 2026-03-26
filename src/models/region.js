dc2df3b677fabb085482ba0e743c757f35877ea2
Dmytro Kitseliuk <dima_kitseliuk@ukr.net>
2026-03-26 11:28:27 +0200

    add validation constants

--- src/models/region.js
+++ src/models/region.js
@@ -1,31 +1,33 @@
 import { Schema, model } from 'mongoose';
-import { REGION_LEVEL } from '../constants/regionLevel.js';
+import { REGION_LEVEL } from '../constants/validation.js';
 
-const regionSchema = new Schema({
-  region: {
-    type: String,
-    required: true,
-    trim: true,
+const regionSchema = new Schema(
+  {
+    region: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    level: {
+      type: String,
+      required: true,
+      enum: REGION_LEVEL,
+    },
+    note: {
+      type: String,
+      trim: true,
+    },
   },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-  },
-  level: {
-    type: String,
-    required: true,
-    enum: REGION_LEVEL,
-  },
-  note: {
-    type: String,
-    trim: true,
-  },
-},
   {
     timestamps: true,
-  versionKey: false,
-});
+    versionKey: false,
+  },
+);
 
 export const Region = model('Region', regionSchema);
