c7f6d9d55262cd1b788b8323ce168dbfe2eed8df
Yaroslav <yazik.tariz@gmail.com>
2026-03-27 18:58:35 +0200

    fix conflict

--- src/controllers/locationsController.js
+++ src/controllers/locationsController.js
@@ -1,7 +1,7 @@
 import { Location } from '../models/location.js';
 import createHttpError from 'http-errors';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
-import { getAllLocations as getAllLocationsService } from '../services/locationsService.js';
+import locationsService from '../services/locationsService.js';
 
 export const getAllLocations = async (req, res) => {
   const { page, perPage, search, region, type, sortBy, sortOrder } = req.query;
@@ -26,25 +26,55 @@ export const getAllLocations = async (req, res) => {
   });
 };
 
-export const updateLocation = async (req, res) => {
+export const getLocationById = async (req, res) => {
   const { locationId } = req.params;
+
+  const location = await locationsService.getLocationById(locationId);
+
+  if (!location) {
+    throw createHttpError(404, 'Location not found');
+  }
+
+  res.status(200).json({ location });
+};
+
+export const createLocation = async (req, res) => {
+  const payload = { ...req.body, ownerId: req.user._id };
 
   if (req.file) {
     const result = await saveFileToCloudinary(req.file.buffer);
-    req.body.image = result.secure_url;
+    payload.image = result.secure_url;
+  } else {
+    throw createHttpError(400, 'No file');
   }
 
-  const updatePayload = { ...req.body };
-
-  if (updatePayload.type) {
-    updatePayload.locationType = updatePayload.type;
-    delete updatePayload.type;
+  if (payload.type) {
+    payload.locationType = payload.type;
+    delete payload.type;
   }
 
-  const location = await Location.findOneAndUpdate(
+  const location = await locationsService.createLocation(payload);
+
+  res.status(201).json({ location });
+};
+
+export const updateLocation = async (req, res) => {
+  const { locationId } = req.params;
+  const payload = { ...req.body };
+
+  if (req.file) {
+    const result = await saveFileToCloudinary(req.file.buffer);
+    payload.image = result.secure_url;
+  }
+
+  if (payload.type) {
+    payload.locationType = payload.type;
+    delete payload.type;
+  }
+
+  const location = await locationsService.updateLocation(
     { _id: locationId, ownerId: req.user._id },
-    updatePayload,
-    { new: true, runValidators: true },
+    payload,
   );
 
   if (!location) {
